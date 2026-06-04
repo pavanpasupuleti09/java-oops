ba17fcf5af91dfdc43c403d2ed0746f265b45beb
Pavan <your_real_email@gmail.com>
2026-06-04 12:22:50 +0530

    feat: oops

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product product = new Product(); // Product -> class name,  product -> reference variable
+        product.id = 1;
+        product.name = "realme TechLife 80 cm (32 inch) QLED HD Ready Smart Google TV (32HDGQRDDAQ)";
+        product.rating = 4.3F;
+        product.maxRetailPrice = 23999;
+        product.discountPercentage = 55;
+        product.isAvailable = true;
+        product.reviewsCount = 26847;
+        product.company = "realme ";
+        product.category =  "Tv";
+        product.displaySize = 55;
+        product.manufacturedYear = 2026;
+        System.out.println("id :" +product.id);
+        System.out.println("name :" +product.name);
+        System.out.println("Max Retail Price :" +product.maxRetailPrice);
+        System.out.println("Rating :" +product.rating);
+        System.out.println("Discount Percentage :" +product.discountPercentage);
+        System.out.println("Is available :" +product.isAvailable);
+        System.out.println("Reviews count :" +product.reviewsCount);
+        System.out.println("company :" +product.company);
+        System.out.println("category :" +product.category);
+        System.out.println("Display size :" +product.displaySize);
+        System.out.println("manufactured Year :" +product.manufacturedYear);
+        System.out.println("--------------------------------------");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
     }
-}
+    }
