4335de83c947efce0ce15e27e0e75d018266ef1f
“pavanpasupuleti09” <your_real_email@gmail.com>
2026-06-09 23:43:45 +0530

    feat: arrays added

---
+++ src/com/java/arrays/ArraysTest.java
@@ -0,0 +1,18 @@
+package com.java.arrays;
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        int[] ids = new int[5];
+        ids[0]=1;
+        ids[1]=2;
+        ids[2]=3;
+        ids[3]=4;
+        ids[4]=5;
+        System.out.println(ids.length);
+        System.out.println(ids.length-1);
+        System.out.println(ids[2]);
+
+
+
+    }
+}
