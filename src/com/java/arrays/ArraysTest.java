6e83b336b76fe5e2f26fe92f70e551f141fd60e0
“pavanpasupuleti09” <your_real_email@gmail.com>
2026-06-23 18:31:23 +0530

    feat: arrays practise added

--- src/com/java/arrays/ArraysTest.java
+++ src/com/java/arrays/ArraysTest.java
@@ -11,27 +11,36 @@ public class ArraysTest {
         ids[3] = 40;
         ids[4] = 50;
 // for print all elements in single using loop
-    for (int i =0;i < ids.length;i++){
+        for (int i = 0; i < ids.length; i++) {
             System.out.print(ids[i] + " ");
         }
 
-    // for print all elements in single line
+        // for print all elements in single line
         System.out.println(Arrays.toString(ids));
-    // print all elements in a row
-        for(int i = 0; i<ids.length; i++){
+        // print all elements in a row
+        for (int i = 0; i < ids.length; i++) {
             System.out.println(ids[i]);
         }
 // sum of all elements but not recommended
-        System.out.println(ids[0] + ids[1] + ids[2] + ids[3] + ids[4] );// also it works but large data take time to take all so we use loops
+        System.out.println(ids[0] + ids[1] + ids[2] + ids[3] + ids[4]);// also it works but large data take time to take all so we use loops
 // sum of all elements
         int sum = 0;
-        for ( int i = 0;i < ids.length; i++){
-          sum=sum + ids[i];
+        for (int i = 0; i < ids.length; i++) {
+            sum = sum + ids[i];
 
         }
         System.out.println(sum);
+        // Average of all elements
+        int sum1 = 0;
+
+        for (int i = 0; i < ids.length; i++) {
+            sum1 = sum1 + ids[i];
 
 
 
+        }
+        double average = (double) sum1 / ids.length;
+
+        System.out.println("Average = " + average);
     }
 }
