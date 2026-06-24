a8f5a64cc58f2eae476abe113e2a230d30b4c9d6
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-24 17:39:51 +0530

    feat: add string print first character program

---
+++ src/com/java/strings/PrintCharacters.java
@@ -0,0 +1,8 @@
+package com.java.strings;
+
+public class PrintCharacters {
+    public static void main(String[] args) {
+        String input = "Java";
+        System.out.println(input.charAt(0));
+    }
+}
