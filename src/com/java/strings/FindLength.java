398a3b581e5322a58a7de95a13f7b382da1e4367
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-24 17:34:23 +0530

    feat: add string length program

---
+++ src/com/java/strings/FindLength.java
@@ -0,0 +1,8 @@
+package com.java.strings;
+
+public class FindLength {
+    public static void main(String[] args) {
+        String input = "Apple";
+        System.out.println(input.length());
+    }
+}
