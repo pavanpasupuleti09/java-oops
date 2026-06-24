d23b6f5920575e7e86fe2a1507659aec002e0d16
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-24 17:28:36 +0530

    feat: stringtest added

---
+++ src/com/java/strings/StringTest.java
@@ -0,0 +1,10 @@
+package com.java.strings;
+
+public class StringTest {
+    public static void main(String[] args) {
+       String name = "Amazon";
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(3));
+
+    }
+}
