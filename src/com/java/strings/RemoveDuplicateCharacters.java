e95e05feaf5da396c96767e3a2e812ff79f65c50
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-26 21:59:13 +0530

    feat: add solutions for basic string problems

---
+++ src/com/java/strings/RemoveDuplicateCharacters.java
@@ -0,0 +1,21 @@
+package com.java.strings;
+
+public class RemoveDuplicateCharacters {
+    public static void main(String[] args) {
+        String str = "programming";
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++){
+
+            char ch = str.charAt(i);
+
+            if(result.indexOf(ch) == -1){
+                result += ch;
+            }
+        }
+
+        System.out.println(result);
+    }
+    }
+
