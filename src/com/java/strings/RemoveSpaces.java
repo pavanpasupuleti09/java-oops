aa2b2fec9490ea7273d3c5ad336993f45a0a9378
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-26 21:53:51 +0530

    feat: add solutions for basic string problems

---
+++ src/com/java/strings/RemoveSpaces.java
@@ -0,0 +1,12 @@
+package com.java.strings;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Java Full Stack";
+
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+    }
+}
+
