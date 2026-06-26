34069708ae931b0c5a4d405be79acb0fda165572
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-26 21:54:56 +0530

    feat: add solutions for basic string problems

---
+++ src/com/java/strings/UppercaseToLowercase.java
@@ -0,0 +1,10 @@
+package com.java.strings;
+
+public class UppercaseToLowercase {
+    public static void main(String[] args) {
+
+        String str = "HELLO JAVA";
+
+        System.out.println(str.toLowerCase());
+    }
+}
